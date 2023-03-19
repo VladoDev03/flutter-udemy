731feccd896b30cff3771ceea34035d2bf8f003f
Vladimir Stoyanov <vladsto101@gmail.com>
2023-03-19 23:23:38 +0200

    Added basic logic to the app.

--- dart_basics/classes.dart
+++ dart_basics/classes.dart
@@ -1,14 +1,27 @@
 void main(List<String> args) {
-  Person me = Person('Vladimir', 20);
+  // Person me = Person('Vladimir', 20);
+  Person me = Person(name: 'Vladimir', age: 20);
   print('${me.name} -> ${me.age}');
 }
 
 class Person {
-  String name = '';
-  int age = 0;
+  String? name;
+  int? age;
 
-  Person(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
+  // Person(String name, int age) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  // Person({@required String? name, int? age}) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  // Person({String? name, int? age}) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  Person({this.name, this.age}) {}
 }
