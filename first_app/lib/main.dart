054b1f80c0359b15bc31e21db4e0d728cdac8a9e
Vladimir Stoyanov <vladsto101@gmail.com>
2023-03-20 19:36:37 +0200

    Custom widget.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,16 +10,16 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
 
     print('Answer 1 chosen!');
@@ -37,20 +39,20 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Question(questions[_questionIndex]),
             ElevatedButton(
-              onPressed: answerQuestion,
-              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+              child: const Text('Answer 1'),
             ),
             ElevatedButton(
               onPressed: () => print("Answer 2 chosen!"),
-              child: Text('Answer 2'),
+              child: const Text('Answer 2'),
             ),
             ElevatedButton(
               onPressed: () {
                 print("Answer 3 chosen!");
               },
-              child: Text('Answer 3'),
+              child: const Text('Answer 3'),
             ),
           ],
         ),
