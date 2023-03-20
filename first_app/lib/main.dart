f2afd6811a934641eeda869a18bf6b0ace4f504f
Vladimir Stoyanov <vladsto101@gmail.com>
2023-03-20 19:05:16 +0200

    Stateful widgets.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,32 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print('Answer 1 chosen!');
+  }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
         body: Column(
-          children: const [
-            Text('The question'),
+          children: [
+            Text(questions[questionIndex]),
             ElevatedButton(
-              onPressed: null,
+              onPressed: answerQuestion,
               child: Text('Answer 1'),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: () => print("Answer 2 chosen!"),
               child: Text('Answer 2'),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: () {
+                print("Answer 3 chosen!");
+              },
               child: Text('Answer 3'),
             ),
           ],
