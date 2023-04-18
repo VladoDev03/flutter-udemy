53f5f03ca105dab2c78a25080d9fe8e9c98b8303
Vladimir Stoyanov <vladsto101@gmail.com>
2023-04-19 01:56:00 +0300

    Finished most of the native device features section.

--- favorite_places_app/lib/screens/places_screen.dart
+++ favorite_places_app/lib/screens/places_screen.dart
@@ -6,11 +6,24 @@ import 'package:favorite_places_app/screens/add_place_screen.dart';
 import 'package:favorite_places_app/widgets/places_list.dart';
 import 'package:favorite_places_app/providers/user_places_provider.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _PlacesScreenState();
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
@@ -30,11 +43,14 @@ class PlacesScreen extends ConsumerWidget {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: PlacesList(
-          places: userPlaces,
-        ),
-      ),
+          padding: const EdgeInsets.all(8),
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(child: CircularProgressIndicator())
+                    : PlacesList(places: userPlaces),
+          )),
     );
   }
 }
