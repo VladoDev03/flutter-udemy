b3be2084aa6c5e191a95594463d7f3b559a7ddaa
Vladimir Stoyanov <vladsto101@gmail.com>
2024-01-26 23:45:12 +0200

    Added basic flutter app.

---
+++ roll-dice-app/lib/widgets/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:basics/widgets/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello, World!'),
+      ),
+    );
+  }
+}
